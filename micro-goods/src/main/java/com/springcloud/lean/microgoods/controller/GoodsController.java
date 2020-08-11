246d34fb36afd5afdefc7d83201b9fa433215f12
hemolin <839021699@qq.com>
2020-08-11 21:45:55 +0800

    xc

--- micro-goods/src/main/java/com/springcloud/lean/microgoods/controller/GoodsController.java
+++ micro-goods/src/main/java/com/springcloud/lean/microgoods/controller/GoodsController.java
@@ -3,6 +3,7 @@ package com.springcloud.lean.microgoods.controller;
 import com.springcloud.lean.microgoods.service.GoodsService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +14,12 @@ public class GoodsController {
     @Autowired
     private GoodsService goodsService;
 
+    @Value("${server.port}")
+    String port;
+
     @RequestMapping("queryGoods")
     public String queryGoods() {
+        log.info(String.format("queryGoods: port=%s", port));
         return goodsService.queryGoods();
     }
 }
