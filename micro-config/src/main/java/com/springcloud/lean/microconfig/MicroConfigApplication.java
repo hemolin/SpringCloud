6c4b93a504e4b9b21cd5ac58f43b71a7cd0fff2a
hemolin <839021699@qq.com>
2020-08-14 16:03:06 +0800

    xc

--- micro-config/src/main/java/com/springcloud/lean/microconfig/MicroConfigApplication.java
+++ micro-config/src/main/java/com/springcloud/lean/microconfig/MicroConfigApplication.java
@@ -2,10 +2,13 @@ package com.springcloud.lean.microconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableConfigServer
 @EnableEurekaClient
+//@EnableHystrixDashboard
 public class MicroConfigApplication {
 
     public static void main(String[] args) {
--- micro-goods/src/main/java/com/springcloud/lean/microgoods/controller/GoodsController.java
+++ micro-goods/src/main/java/com/springcloud/lean/microgoods/controller/GoodsController.java
@@ -4,22 +4,34 @@ import com.springcloud.lean.microgoods.service.GoodsService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@RefreshScope
 public class GoodsController {
 
     @Autowired
     private GoodsService goodsService;
 
+    @Autowired
+    Environment environment;
+
     @Value("${server.port}")
     String port;
+    @Value("${username}")
+    String username;
+//    @Value("${passwd}")
+//    String passwd;
 
     @RequestMapping("queryGoods")
     public String queryGoods() {
         log.info(String.format("queryGoods: port=%s", port));
+        log.info(String.format("environment->username=%s", environment.getProperty("username")));
+//        log.info(String.format("@Value->username=%s, passwd=%s", username, passwd));
         return goodsService.queryGoods();
     }
 }
